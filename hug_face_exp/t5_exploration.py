cd2e668243027fe087291f55d560d3328315a55a
karthikrbabu <karthikrbabu@berkeley.edu>
2021-03-04 08:46:13 +0000

    groundwork all laid for T5 modelling

--- hug_face_exp/t5_exploration.py
+++ hug_face_exp/t5_exploration.py
@@ -14,6 +14,20 @@
 #     name: python3
 # ---
 
+# # T5 Model Exploration
+#
+# Goal of this notebook is to cleanly setup a barebone T5 Model Pipeline to generate text summaries form our meaning representations. Should include the following steps
+#
+# * Import Libraries
+# * Load Train/Dev/Test Data
+# * Config Definitions
+# * Pre-process Data (Tensors)
+# * Quick Tensor EDA
+# * Tensorboard Loading
+# * Optimizer Init
+# * Train Model
+# * Evaluate Model
+
 # +
 #Utilities
 import pandas as pd
@@ -25,7 +39,6 @@ import re
 import os
 import datetime
 import time
-
 import collections
 from collections import defaultdict
 
@@ -43,6 +56,7 @@ from transformers import (TFAutoModelWithLMHead, AutoTokenizer,
 
 # Tensorflow
 import tensorflow as tf
+# tf.enable_eager_execution()
 
 #PyTorch
 import torch
@@ -53,74 +67,266 @@ import wandb
 # %load_ext tensorboard
 # -
 
+print(tf.__version__)
+
+# ### Setup Directories
+
+data_dir = "./data"
+log_dir = f"{data_dir}/experiments/t5/logs"
+save_path = f"{data_dir}/experiments/t5/models"
+cache_path_train = f"{data_dir}/cache/t5.train"
+cache_path_test = f"{data_dir}/cache/t5.test"
+
 # ### Load Data 
 
-# +
 train = pd.read_csv('../data/e2e-cleaning-master/cleaned-data/train-fixed.no-ol.csv').drop(["fixed","orig_mr"], axis=1)
 dev = pd.read_csv('../data/e2e-cleaning-master/cleaned-data/devel-fixed.no-ol.csv').drop(["fixed","orig_mr"], axis=1) 
 test = pd.read_csv('../data/e2e-cleaning-master/cleaned-data/test-fixed.csv').drop(["fixed","orig_mr"], axis=1)
 print("Train Size", train.shape)
 print("Dev Size", dev.shape)
 print("Test Size", test.shape)
-
 train.head()
 
+# ### Configs
+
 # +
-tokenizer = AutoTokenizer.from_pretrained("t5-small")
+#Configs
+config = {
+    'model_size': "t5-small"
+}
 
-t5_config = T5Config(vocab_size=32100, 
-                     d_model=512,
-                     d_kv=64, 
-                     d_ff=2048,
-                     num_layers=6,
-                     num_decoder_layers=None,
-                     num_heads=8, 
-                     relative_attention_num_buckets=32,
-                     dropout_rate=0.1, 
-                     layer_norm_epsilon=1e-06, 
-                     initializer_factor=1.0, 
-                     feed_forward_proj='relu',
-                     is_encoder_decoder=True,
-                     use_cache=True, 
-                     pad_token_id=0, 
-                     eos_token_id=1)
+model_config = {
+    'vocab_size':32100,
+    'd_model':512,
+    'd_kv':64,
+    'd_ff':2048,
+    'num_layers': 6,
+    'num_decoder_layers':None,
+    'num_heads':8,
+    'relative_attention_num_buckets':32,
+    'dropout_rate':0.1,
+    'layer_norm_epsilon':1e-06,
+    'initializer_factor':1.0,
+    'feed_forward_proj':'relu',
+    'is_encoder_decoder':True,
+    'use_cache':True,
+    'pad_token_id': 0,
+    'eos_token_id': 1
+}
 
-t5_layer = TFT5ForConditionalGeneration.from_pretrained('t5-small')
-
-
+token_config = {
+    'max_length':None,
+    'max_target_length':None,
+    'padding':'longest',
+    'return_tensors': 'tf',
+    'truncation':True,
+}
 # -
+
+# ### Initializations
+
+# +
+tokenizer = AutoTokenizer.from_pretrained(config['model_size'])
+t5_config = T5Config(**token_config)
+t5_layer = TFT5ForConditionalGeneration.from_pretrained(config['model_size'])
 
 #Vocab Length
-len(tokenizer.vocab)
+print("Vocab Length: ", len(tokenizer.vocab))
+
 
 # +
-# %%time
+def convert_pd_to_tf_dataset(data):
+    """
+    Function responsible for taking our Dataset and converting it to Tensorflow Dataset Object
+    https://medium.com/when-i-work-data/converting-a-pandas-dataframe-into-a-tensorflow-dataset-752f3783c168
+    """
+    
+    features = ['mr']
+    converted_dataset = (
+        tf.data.Dataset.from_tensor_slices(
+            (
+                tf.cast(data[features].values, tf.float32),
+                tf.cast(data['ref'].values, tf.int32)
+            )
+        )
+    )
+    
+    return converted_dataset
 
-#Process data for training 
-batch_encoding= tokenizer.prepare_seq2seq_batch(src_texts=list(train['mr']), 
-                                             tgt_texts=list(train['ref']),
-                                             max_length=None,
-                                             max_target_length=None,
-                                             padding='longest',
-                                             return_tensors='tf',
-                                             truncation=True)
-batch_encoding.keys()
+
+def encode(data, tokenizer, token_config):
+    """
+    Pre-process all data thats passed through BatchEncoding
+    * data - 2 columns ['mr','ref']
+    * tokenizer - T5 or Autotokenizer
+    * token_config - Config to define encoding options
+    """
+    df = data.copy()
+    
+    #Add 'summary' task prefix to the input
+    df.mr = 'summarize: ' + df.mr
+
+    #Process data for training 
+    batch_encoding= tokenizer.prepare_seq2seq_batch(src_texts=list(df['mr']), 
+                                                 tgt_texts=list(df['ref']),
+                                                 **token_config)
+    input_ids = batch_encoding['input_ids']
+    input_attention_mask = batch_encoding['attention_mask']
+    label_ids = batch_encoding['labels']
+    
+    return {'input_ids':input_ids, 'attention_mask': input_attention_mask, 'labels':label_ids}
+
+
+#[WIP] - might not be needed
+def encode_tf(data, tokenizer, token_config):
+    encoded = encode(data, tokenizer, token_config)
+    return (encoded, None)
+
+
+#[WIP] - might not be needed
+def create_dataset(source_dataset, tokenizer, token_config, 
+                   cache_path=None, batch_size=4, 
+                   buffer_size= 1000, shuffling=True):
+
+    dataset = encode_tf(source_dataset, tokenizer, token_config)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    return dataset
+    
+
+
 # -
 
-print("Input Padded Length: ", len(batch_encoding.input_ids[0]))
-print("Output Padded Length: ", len(batch_encoding.labels[0]))
+# ### Process Train
 
-train_mrs_token_counts = pd.DataFrame({'counts': [np.count_nonzero(mask) for mask in batch_encoding.attention_mask]})
+#Pre Process Training Data
+train_preprop = encode(train, tokenizer, token_config)
+train_input_ids, train_attention_mask, train_labels = train_preprop['input_ids'], train_preprop['attention_mask'], train_preprop['labels']
+print("Input Padded Length: ", len(train_input_ids[0]))
+print("Output Padded Length: ", len(train_labels[0]))
+
+train_mrs_token_counts = pd.DataFrame({'counts': [np.count_nonzero(mask) for mask in train_attention_mask]})
 print(stats.describe(train_mrs_token_counts['counts']))
 fig = px.histogram(train_mrs_token_counts, x="counts", histnorm='percent') # histnorm: percent, probability, density
 fig.update_layout(title_text="Histogram: Train Mrs Token Counts")
 fig.show()
 
-train_labels_token_counts = pd.DataFrame({'counts': [np.count_nonzero(label) for label in batch_encoding.labels]})
+train_labels_token_counts = pd.DataFrame({'counts': [np.count_nonzero(label) for label in train_labels]})
 print(stats.describe(train_labels_token_counts['counts']))
 fig = px.histogram(train_labels_token_counts, x="counts", histnorm='percent') # histnorm: percent, probability, density
 fig.update_layout(title_text="Histogram: Train Labels Token Counts")
 fig.show()
+
+
+# ### Custom Learning Rate Optimizer
+
+# +
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    """
+    Class created to optimize the learning rate moderation to start steep, then become gradual
+    """
+    def __init__(self, warmup_steps=1e4):
+        super().__init__()
+
+        self.warmup_steps = tf.cast(warmup_steps, tf.float32)
+    
+    def __call__(self, step):
+        step = tf.cast(step, tf.float32)
+        m = tf.maximum(self.warmup_steps, step)
+        m = tf.cast(m, tf.float32)
+        lr = tf.math.rsqrt(m)
+    
+        return lr 
+
+#Example
+plt.style.use('ggplot')
+schedule = CustomSchedule()
+plt.plot(schedule(tf.range(25000, dtype=tf.float32)))
+plt.xlabel("Steps")
+plt.ylabel("Learning rate")
+# -
+
+# ### Setup Logging - TensorBoard
+
+# +
+start_profile_batch = steps+10
+stop_profile_batch = start_profile_batch + 100
+profile_range = f"{start_profile_batch},{stop_profile_batch}"
+
+log_path = log_dir + "/" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_path, histogram_freq=1,
+                                                     update_freq=20,profile_batch=profile_range)
+
+checkpoint_filepath = save_path + "/" + "T5-{epoch:04d}-{val_loss:.4f}.ckpt"
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=False,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+
+callbacks = [tensorboard_callback, model_checkpoint_callback] 
+metrics = [tf.keras.metrics.SparseTopKCategoricalAccuracy(name='accuracy') ]
+# -
+
+# ### Train Parameters
+#
+
+warmup_steps = 1e4
+batch_size = 4
+encoder_max_len = 250
+decoder_max_len = 54
+buffer_size = 1000
+ntrain = train.shape[0]
+nvalid = dev.shape[0]
+steps = int(np.ceil(ntrain/batch_size))
+valid_steps = int(np.ceil(nvalid/batch_size))
+print("Total Steps: ", steps)
+print("Total Validation Steps: ", valid_steps)
+
+# ### Optimizer
+
+learning_rate = CustomSchedule(warmup_steps)
+# learning_rate = 0.001  # Instead set a static learning rate
+optimizer = tf.keras.optimizers.Adam(learning_rate)
+
+# ### Process Dev
+
+#Pre Process Training Data
+dev_preprop = encode(dev, tokenizer, token_config)
+dev_input_ids, dev_attention_mask, dev_labels = dev_preprop['input_ids'], dev_preprop['attention_mask'], dev_preprop['labels']
+print("Input Padded Length: ", len(dev_input_ids[0]))
+print("Output Padded Length: ", len(dev_labels[0]))
+
+# ### TensorBoard
+
+# %tensorboard --logdir ./data/experiments/t5/logs
+
+# ### Train Model
+
+t5_layer.compile(optimizer=optimizer, metrics=metrics)
+
+epochs_done = 0
+t5_layer.fit(train_ds, epochs=5, steps_per_epoch=steps, callbacks=callbacks, 
+          validation_data=valid_ds, validation_steps=valid_steps, initial_epoch=epochs_done)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # ## Example #1
 
@@ -147,48 +353,7 @@ result = model.generate(inputs)
 
 result.numpy()[0]
 
-print(result)
-print(tokenizer.convert_ids_to_tokens(result.numpy()[0]))
 
 
-
-# +
-# Creating a custom dataset for reading the dataframe and loading it into the dataloader to pass it to the neural network at a later stage for finetuning the model and to prepare it for predictions
-
-class CustomDataset(Dataset):
-    
-    def __init__(self, dataframe, tokenizer, source_len, summ_len):
-        self.tokenizer = tokenizer
-        self.data = dataframe
-        self.source_len = source_len
-        self.summ_len = summ_len
-        self.text = self.data.text
-        self.ctext = self.data.ctext
-
-    def __len__(self):
-        return len(self.text)
-
-    def __getitem__(self, index):
-        ctext = str(self.ctext[index])
-        ctext = ' '.join(ctext.split())
-
-        text = str(self.text[index])
-        text = ' '.join(text.split())
-
-        source = self.tokenizer.batch_encode_plus([ctext], max_length= self.source_len, pad_to_max_length=True,return_tensors='pt')
-        target = self.tokenizer.batch_encode_plus([text], max_length= self.summ_len, pad_to_max_length=True,return_tensors='pt')
-
-        source_ids = source['input_ids'].squeeze()
-        source_mask = source['attention_mask'].squeeze()
-        target_ids = target['input_ids'].squeeze()
-        target_mask = target['attention_mask'].squeeze()
-
-        return {
-            'source_ids': source_ids.to(dtype=torch.long), 
-            'source_mask': source_mask.to(dtype=torch.long), 
-            'target_ids': target_ids.to(dtype=torch.long),
-            'target_ids_y': target_ids.to(dtype=torch.long)
-        }
-# -
 
 
