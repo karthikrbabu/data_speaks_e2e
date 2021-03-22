b1fd5255aec5cf55c61b0be7a3bea09c87459311
Praveen Kasireddy <praveenr.qa@gmail.com>
2021-03-22 00:25:36 -0700

    updated the code to handle headers

--- src/utils/data_builder.py
+++ src/utils/data_builder.py
@@ -4,18 +4,19 @@ import re
 from csv import reader, writer
 import random
 
-
 # open file in read mode
-
 
 def remove_tags():
     with open(os.path.abspath(os.path.join(os.getcwd(), "../../data/e2e-dataset/testset_w_refs.csv")), 'r') as read_obj:
         with open(os.path.abspath(os.path.join(os.getcwd(), "../../data/e2e-dataset/testset_w_refs_notags.csv")), mode='w') as write_obj:
             # pass the file object to reader() to get the reader object
             csv_reader = reader(read_obj)
+            headers = next(csv_reader)
             csv_writer = writer(write_obj,delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+            csv_writer.writerow(headers)
             # Iterate over each row in the csv using reader object
             print("Line Number", csv_reader.line_num)
+            print("first record", csv_reader.line_num)
             for row in csv_reader:
                 # row variable is a list that represents a row in csv
                 mr = row[0].split(",")
@@ -33,7 +34,9 @@ def random_features():
         with open(os.path.abspath(os.path.join(os.getcwd(), "../../data/e2e-dataset/testset_w_refs_random.csv")), mode='w') as write_obj:
             # pass the file object to reader() to get the reader object
             csv_reader = reader(read_obj)
-            csv_writer = writer(write_obj,delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+            headers = next(csv_reader)
+            csv_writer = writer(write_obj, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+            csv_writer.writerow(headers)
             # Iterate over each row in the csv using reader object
             print("Line Number", csv_reader.line_num)
             for row in csv_reader:
